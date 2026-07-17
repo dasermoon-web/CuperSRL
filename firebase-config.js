c38fa08cbccecce9e8c72ac40decc2c80f9e1836
dasermoon-web <dasermoon@gmail.com>
2026-07-17 18:00:27 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+﻿const firebaseConfig = {
+  apiKey: "AIzaSyDCb5pmxGGa5HyHVrcs2uO2TWHz1YMfnx4",
+  authDomain: "cupersrl-dd101.firebaseapp.com",
+  projectId: "cupersrl-dd101",
+  storageBucket: "cupersrl-dd101.firebasestorage.app",
+  messagingSenderId: "997420526173",
+  appId: "1:997420526173:web:0de00b1d2cf4fe76243bb4"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
